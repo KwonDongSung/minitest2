aa8836fa15a787fd228d2021e98f4828f681e933
KwonDongSung <21700045@handong.edu>
2020-04-24 23:18:26 +0900

    added savingfile

--- manager.c
+++ manager.c
@@ -40,8 +40,10 @@ void saveData(Product p[], int count){
 
 	//중량 가격 제품명
 	fp= fopen("product.txt","wt");
-	
-	
+	for(int i=0;i<count ; i++){
+	if(p[i].price != -1)
+	fprintf(fp, "%d %d %s\n",p[i].weight,p[i].price,p[i].name);
+	}
 	fclose(fp);
 	printf("저장됨!\n");
 }
@@ -53,7 +55,7 @@ int loadData(Product *p){
 	FILE*fp;
 
 	//파일 내용을 읽어와서 배열에 값 추가하기
-
+	
 
 
 
